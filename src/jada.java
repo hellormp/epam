e77e5b9d35de800e9daeb76a7e4594f325548d84
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-02-13 13:32:14 +0200

    remove unusless classes

--- src/jada.java
+++ src/jada.java
@@ -1,29 +1,36 @@
-import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-
 public class jada {
-
-
+    private int age;
     public static void main(String[] args) {
-        int []mas = new int[10];
 
-        for (int i = 0; i < mas.length ; i++) {
-            mas[i] = (int)Math.round(Math.random()*10);
-            System.out.println(mas[i]);
-            
+    setDay("dad");
+
+    }
+
+    public static void setDay(String day){
+
+                System.out.println();
         }
 
+    }
+
+    class dadgfd{
+    dadgfd dadgfd = new dadgfd();
 
 
+    }
+class Human{
+    int age;
+    int weight;
+    int height;
 
-
-
-
-
-
-
+    Human(int age, int weight){
+        this.age = age;
+        this.weight = weight;
+    }
+    Human(int age, int weight, int height){
+        //вы вызываете конструктор с двумя параметрами
+        this(age, weight);
+        //и добавляете недостающую переменную
+        this.height = height;
     }
 }
