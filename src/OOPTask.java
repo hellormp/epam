8cf593ea35b86375c52eacd790f599e4f13b32b8
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-04-03 16:17:25 +0300

    CC

--- src/OOPTask.java
+++ src/OOPTask.java
@@ -1,21 +1,17 @@
 import java.util.Vector;
 import java.util.Random;
 public class OOPTask {
-
     private final double cordenateX;
     private final double cordenateY;
     private final double cordenateZ;
-
     public OOPTask(double x, double y, double z){
         cordenateX = x;
         cordenateY = y;
         cordenateZ = z;
-        
     }
     public double getLengthOf3DVectors(double x, double y, double z){
         return Math.sqrt(Math.pow(cordenateX,2)+Math.pow(cordenateY,2)+Math.pow(cordenateZ,2));
     }
-
     static Vector<String> vector;
     static void getRandomElements()
     {
@@ -28,7 +24,6 @@ public class OOPTask {
     }
     public static void main(String[] args)
     {
-
         vector = new Vector<String>();
         vector.add("Welcome");
         vector.add("To");
