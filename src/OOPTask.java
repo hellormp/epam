afd1d8270fba1fa3367734f6458f0f8d7fb07edd
“Yaroslav <Yaroslav_Kosovchenko@epam.com>
2022-04-03 16:09:12 +0300

    1 and 2 task done!

---
+++ src/OOPTask.java
@@ -0,0 +1,45 @@
+import java.util.Vector;
+import java.util.Random;
+public class OOPTask {
+
+    private final double cordenateX;
+    private final double cordenateY;
+    private final double cordenateZ;
+
+    public OOPTask(double x, double y, double z){
+        cordenateX = x;
+        cordenateY = y;
+        cordenateZ = z;
+        
+    }
+    public double getLengthOf3DVectors(double x, double y, double z){
+        return Math.sqrt(Math.pow(cordenateX,2)+Math.pow(cordenateY,2)+Math.pow(cordenateZ,2));
+    }
+
+    static Vector<String> vector;
+    static void getRandomElements()
+    {
+        Random random = new Random();
+        for (int i = 0; i < vector.size(); i++)
+        {
+            int index = random.nextInt(vector.size());
+            System.out.println(vector.get(index));
+        }
+    }
+    public static void main(String[] args)
+    {
+
+        vector = new Vector<String>();
+        vector.add("Welcome");
+        vector.add("To");
+        vector.add("Geeks");
+        vector.add("For");
+        vector.add("Geeks");
+        getRandomElements();
+    }
+}
+
+
+
+
+
